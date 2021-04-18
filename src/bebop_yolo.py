34872c956350f3523464b226797adb9ece8cb132
Tanguyvans <71881765+Tanguyvans@users.noreply.github.com>
2021-04-18 15:36:29 +0200

    Update bebop_yolo.py
    distingue les différentes personnes

--- src/bebop_yolo.py
+++ src/bebop_yolo.py
@@ -14,8 +14,13 @@ from cv_bridge import CvBridge, CvBridgeError
 
 bridge = CvBridge()
 
+# dimension de l'objet
 width = 0
 height = 0
+
+# position precedente de l'objet
+objCenterX = 0
+objCenterY = 0
 
 rowWindow = 0
 colWindow = 0
@@ -44,66 +49,113 @@ def Bounding_Callback(msg):
     global width 
     global height 
 
-    #print(rowWindow, colWindow)
+    global objCenterX
+    global objCenterY
+
+    #recupere le milieu de l'image
     x_middle_frame = colWindow/2
     y_middle_frame = rowWindow/2
 
-    y_good_height_frame = rowWindow*0.35
+    # hauteur souhaitee 
+    y_good_heigh_frame = rowWindow*0.35
 
+    #initialise les listes contenant les personnes
+    listPerson = []
+    listPosition = []
+
+    # On recupere l'ensemble des personnes avec leurs positions 
     for box in msg.bounding_boxes:
         if (box.Class == "person"):
+            #rospy.loginfo(box.Class)
+            #rospy.loginfo("Xmin {} Xmax {} Ymin {} Ymax {}".format(box.xmin, box.xmax, box.ymin, box.ymax))
 
-            width0 = box.xmax-box.xmin
-            height0 = box.ymax-box.ymin
+            listPosition.append(box.xmax) 
+            listPosition.append(box.xmin) 
+            listPosition.append(box.ymax)
+            listPosition.append(box.ymin)
 
-            if (width==0 and height==0):
-                width = box.xmax-box.xmin
-                height = box.ymax-box.ymin
+            listPerson.append(listPosition)
+            listPosition = []
 
-            # Position voulue - position actuel 
-            x_difference = width - width0
-            y_difference = height - height0
+    if listPerson : 
+        centerX = colWindow/2
+        centerY = rowWindow/2
+        DistCenter = 1000000
+        posePerson = 0
 
-            # milieu en fonction de x
-            x_middle_person = (box.xmax +box.xmin )/2
-            x_rot_difference = x_middle_frame - x_middle_person
+        if (width==0 and height==0):
+            for i in range(len(listPerson)): 
+                # position du centre de l'objet dans l'espace
+                centerX0 = (listPerson[i][0] +listPerson[i][1] )/2
+                centerY0 = (listPerson[i][2] +listPerson[i][3] )/2
+                # distance entre l'objet et le centre de l'image
+                DistCenter0 = (math.sqrt((centerX-centerX0)**2+(centerY-centerY0)**2))
 
-            #calcul en fonction de Z
-            z_difference = (box.ymin-y_good_height_frame)
+                if (DistCenter > DistCenter0):
+                    DistCenter = DistCenter0
+                    posePerson = i
             
+            # declare les dimensions de l'images
+            width = listPerson[posePerson][0]-listPerson[posePerson][1]
+            height = listPerson[posePerson][2]-listPerson[posePerson][3]
 
+            objCenterX = (listPerson[posePerson][0] +listPerson[posePerson][1] )/2
+            objCenterY = (listPerson[posePerson][2] +listPerson[posePerson][3] )/2
 
-            #initialise pub et msg
-            velocity_publisher = rospy.Publisher("/bebop/cmd_vel", Twist, queue_size=1)
-            velocity_message = Twist()
+        else : 
+            for i in range(len(listPerson)): 
+                centerX0 = (listPerson[i][0] +listPerson[i][1] )/2
+                centerY0 = (listPerson[i][2] +listPerson[i][3] )/2
+                DistCenter0 = (math.sqrt((objCenterX-centerX0)**2+(objCenterY-centerY0)**2))
+                if (DistCenter > DistCenter0):
+                    DistCenter = DistCenter0
+                    posePerson = i
 
-            # deplacement selon X
-            if ((abs(y_difference)-10) > 0):
-                x_speed = round(y_difference*0.001, 1)
-                #print(x_speed)
-                velocity_message.linear.x = x_speed
-            else:
-                velocity_message.linear.x = 0
+            objCenterX = (listPerson[posePerson][0] +listPerson[posePerson][1] )/2
+            objCenterY = (listPerson[posePerson][2] +listPerson[posePerson][3] )/2
 
-            # orientation du drone
-            if ((abs(x_rot_difference)-20) > 0):
-                rot_speed = round(x_rot_difference*0.001, 1)
-                velocity_message.angular.z = rot_speed
-            else: 
-                velocity_message.angular.z = 0
+        width0 = listPerson[posePerson][0]-listPerson[posePerson][1]
+        height0 = listPerson[posePerson][2]-listPerson[posePerson][3]
 
-            # hauteur du drone 
-            if (abs(z_difference)-10 > 0):
-                z_speed = round(z_difference*0.001,1)
-                print("Z",-z_speed)
-                velocity_message.linear.z = -z_speed
-            else:
-                velocity_message.linear.z = 0
+        # Position voulue - position actuel 
+        x_difference = width - width0
+        y_difference = height - height0
 
-            velocity_publisher.publish(velocity_message)
+        # milieu en fonction de x
+        x_middle_person = (listPerson[posePerson][0] +listPerson[posePerson][1] )/2
+        x_rot_difference = x_middle_frame - x_middle_person
+
+        #calcul en fonction de Z
+        z_difference = (listPerson[posePerson][3]-y_good_heigh_frame)
+        
+        #initialise pub et msg
+        velocity_publisher = rospy.Publisher("/bebop/cmd_vel", Twist, queue_size=1)
+        velocity_message = Twist()
+
+        # deplacement selon X
+        if ((abs(y_difference)-10) > 0):
+            x_speed = round(y_difference*0.001, 1)
+            #print(x_speed)
+            velocity_message.linear.x = x_speed
+        else:
+            velocity_message.linear.x = 0
+
+        # orientation du drone
+        if ((abs(x_rot_difference)-20) > 0):
+            rot_speed = round(x_rot_difference*0.001, 1)
+            velocity_message.angular.z = rot_speed
+        else: 
+            velocity_message.angular.z = 0
+
+        # hauteur du drone 
+        if (abs(z_difference)-10 > 0):
+            z_speed = round(z_difference*0.001,1)
+            velocity_message.linear.z = -z_speed
+        else:
+            velocity_message.linear.z = 0
+
+        velocity_publisher.publish(velocity_message)
             
-
-
 
 def takeoff():
     pub = rospy.Publisher('/bebop/takeoff', Empty, queue_size=1, latch=True)
